487a6bbb8167b1ae713b881b518e0db9af701868
Allename <allename.dev@gmail.com>
2023-08-25 01:44:03 +0100

    added classes module

--- public/app.js
+++ public/app.js
@@ -1,1 +1,20 @@
-"use strict";
+import { Invoice } from "./classes/Invoice";
+const invOne = new Invoice('Anthony', 'worked on Bluelight website', 120);
+const invTwo = new Invoice('Allename', 'worked on Popu website', 250);
+console.log(invOne, invTwo);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
+const form = document.querySelector('.new-item-form');
+//inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import { Invoice } from "./classes/Invoice"
+
+const invOne = new Invoice('Anthony', 'worked on Bluelight website', 120)
+const invTwo = new Invoice('Allename', 'worked on Popu website', 250)
+console.log(invOne, invTwo)
+
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invoices.forEach(inv => {
+  console.log(inv.client, inv.amount, inv.format())
+})
+
+
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement
+
+//inputs
+const type = document.querySelector('#type') as HTMLSelectElement
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount= document.querySelector('#amount') as HTMLInputElement
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault()
+
+  console.log(
+    type.value,
+    toFrom.value,
+    details.value,
+    amount.valueAsNumber
+  )
+})
